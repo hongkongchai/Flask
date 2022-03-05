93957d4b2e3c698787c0007df4507390762916e5
hongkongchai <hongkong187@163.com>
2022-03-05 17:43:55 +0800

    test05

--- microblog.py
+++ microblog.py
@@ -2,9 +2,5 @@
 from app import app
 
 if __name__=='__main__':
-    print("gitOK")
-    print("okokkoko")
-    print("cxggggg")
-    print("hkhkhkhk")
-    print("测试test")
+
     app.run(debug=True)
