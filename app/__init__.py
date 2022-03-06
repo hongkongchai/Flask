ca75032384c14feecf893dbb898b388edd6c289b
hongkongchai <hongkong187@163.com>
2022-03-06 18:18:45 +0800

    test0306-databases

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python # -*- coding:utf-8 -*-
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from  flask_migrate import Migrate
 app = Flask(__name__)
 print('哪个包或模块在使用main函数',__name__)
 app.config.from_object(Config)
 print('秘钥',app.config['SECRET_KEY'])
 
-from app import routes
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+from app import routes, models
 
 
 
--- microblog.py
+++ microblog.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python # -*- coding:utf-8 -*-
-from app import app
+from app import app,db
+from app.models import User,Post
+
+@app.shell_context_processor
+def mask_shell_context():
+    #创建shell上下文，将数据库实例模型添加到shell会话中
+    return {'db': db, 'User': User, 'Post': Post}
 
 if __name__=='__main__':
 
